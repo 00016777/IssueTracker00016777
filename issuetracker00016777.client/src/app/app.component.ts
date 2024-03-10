b2faf523f7445c1e2546942c9476ac96c8435481
00016777 <wiut00016777@gmail.com>
2024-03-10 16:38:20 +0500

    first commit delete default codes

--- issuetracker00016777.client/src/app/app.component.ts
+++ issuetracker00016777.client/src/app/app.component.ts
@@ -1,12 +1,3 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -14,24 +5,13 @@ interface WeatherForecast {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
+  
+  constructor() {}
 
   ngOnInit() {
-    this.getForecasts();
+    
   }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
+ 
   title = 'issuetracker00016777.client';
 }
