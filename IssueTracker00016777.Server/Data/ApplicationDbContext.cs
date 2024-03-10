30e89f9dda32d1af5aa2343047f2e1febf360f90
00016777 <wiut00016777@gmail.com>
2024-03-10 16:45:22 +0500

    added DbContext and interceptor

---
+++ IssueTracker00016777.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using IssueTracker00016777.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker00016777.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Issue00016777> Issues { get; set; }    
+
+    public DbSet<User00016777> Users { get; set; }  
+
+}
---
+++ IssueTracker00016777.Server/Data/Interceptors/OnSaveInterceptor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using IssueTracker00016777.Models;
+
+namespace IssueTracker00016777.Data.Interceptors;
+
+public class OnSaveInterceptor : SaveChangesInterceptor
+{
+
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public void UpdateEntities(DbContext? context)
+    {
+        if (context == null) return;
+
+        foreach (var entry in context.ChangeTracker.Entries<BaseEntity00016777>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedDate = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+            {
+                entry.Entity.UpdateDate = DateTime.Now;
+            }
+        }
+    }
+
+
+}
